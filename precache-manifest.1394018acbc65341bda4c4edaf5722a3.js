e4595a3c8c1ec4de077a047e6be2992e5b9a751c
Dinis Cruz <dinis.cruz@owasp.org>
2020-10-19 18:47:39 +0100

    Updates

---
+++ precache-manifest.1394018acbc65341bda4c4edaf5722a3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4985f7d22ff4dbc830cc71444ec1e2d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7345f2ce68ad06607991",
+    "url": "/static/css/main.3b98bbeb.chunk.css"
+  },
+  {
+    "revision": "2cc7fcc5fec32b9fb82b",
+    "url": "/static/js/2.952b095d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.952b095d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7345f2ce68ad06607991",
+    "url": "/static/js/main.973dbf26.chunk.js"
+  },
+  {
+    "revision": "c52aa85f0481664c545a",
+    "url": "/static/js/runtime-main.bd76a4c6.js"
+  },
+  {
+    "revision": "3242726a05656dbaa535f9c564cfa3e9",
+    "url": "/static/media/better-kubernetes.3242726a.svg"
+  },
+  {
+    "revision": "7a0f87caa1f0e402af66dc52f537f07d",
+    "url": "/static/media/body-background.7a0f87ca.png"
+  },
+  {
+    "revision": "7c7b1c76a41ef3508a4481931853cb44",
+    "url": "/static/media/built-in-prometheus-stats.7c7b1c76.svg"
+  },
+  {
+    "revision": "b0f068bbb61ed10e07a06e1b50a4cb23",
+    "url": "/static/media/context-aware-terminal.b0f068bb.svg"
+  },
+  {
+    "revision": "82f026e24b5490196a3c079f3d49d061",
+    "url": "/static/media/header-background.82f026e2.png"
+  },
+  {
+    "revision": "780695210c1bc7ce2562588ffbcb0953",
+    "url": "/static/media/header-lens.78069521.png"
+  },
+  {
+    "revision": "4d89dba8a292d974769ba2214b9d2128",
+    "url": "/static/media/logo.4d89dba8.svg"
+  },
+  {
+    "revision": "6cb8bb7999b641b4198498a4ed30d9b2",
+    "url": "/static/media/multi-cluster-management.6cb8bb79.svg"
+  },
+  {
+    "revision": "1e1405756d29efb30ee754ee22909bed",
+    "url": "/static/media/workspaces.1e140575.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/glasswall-desktop/precache-manifest.df44a6ac792df6fdce279a5635733478.js"
+  "/precache-manifest.1394018acbc65341bda4c4edaf5722a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glasswall-desktop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
