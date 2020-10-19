c64f4df6ab0f9bb50ebccdc5e16a240e17fd7279
rajmbcoderx <raj.mb.coderx@gmail.com>
2020-10-19 07:21:38 +0000

    Deploying to gh-pages from  @ 62b23f1fb9d1258a066a3a70863d47247cc8b369 🚀

---
+++ precache-manifest.df44a6ac792df6fdce279a5635733478.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68155f6454e8b87db11052044d5489bd",
+    "url": "/glasswall-desktop/index.html"
+  },
+  {
+    "revision": "d16e865969e5b3361329",
+    "url": "/glasswall-desktop/static/css/main.e9c05e4b.chunk.css"
+  },
+  {
+    "revision": "611a976a72ae9223539d",
+    "url": "/glasswall-desktop/static/js/2.c09c94ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glasswall-desktop/static/js/2.c09c94ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16e865969e5b3361329",
+    "url": "/glasswall-desktop/static/js/main.de28719e.chunk.js"
+  },
+  {
+    "revision": "556d96edf67c6ed0dd5f",
+    "url": "/glasswall-desktop/static/js/runtime-main.ca8a5410.js"
+  },
+  {
+    "revision": "3242726a05656dbaa535f9c564cfa3e9",
+    "url": "/glasswall-desktop/static/media/better-kubernetes.3242726a.svg"
+  },
+  {
+    "revision": "7a0f87caa1f0e402af66dc52f537f07d",
+    "url": "/glasswall-desktop/static/media/body-background.7a0f87ca.png"
+  },
+  {
+    "revision": "7c7b1c76a41ef3508a4481931853cb44",
+    "url": "/glasswall-desktop/static/media/built-in-prometheus-stats.7c7b1c76.svg"
+  },
+  {
+    "revision": "b0f068bbb61ed10e07a06e1b50a4cb23",
+    "url": "/glasswall-desktop/static/media/context-aware-terminal.b0f068bb.svg"
+  },
+  {
+    "revision": "82f026e24b5490196a3c079f3d49d061",
+    "url": "/glasswall-desktop/static/media/header-background.82f026e2.png"
+  },
+  {
+    "revision": "780695210c1bc7ce2562588ffbcb0953",
+    "url": "/glasswall-desktop/static/media/header-lens.78069521.png"
+  },
+  {
+    "revision": "4d89dba8a292d974769ba2214b9d2128",
+    "url": "/glasswall-desktop/static/media/logo.4d89dba8.svg"
+  },
+  {
+    "revision": "6cb8bb7999b641b4198498a4ed30d9b2",
+    "url": "/glasswall-desktop/static/media/multi-cluster-management.6cb8bb79.svg"
+  },
+  {
+    "revision": "1e1405756d29efb30ee754ee22909bed",
+    "url": "/glasswall-desktop/static/media/workspaces.1e140575.svg"
+  }
+]);
