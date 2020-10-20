c6b17a024acb23dc71c2172e2192e46f7da502f0
rajmbcoderx <raj.mb.coderx@gmail.com>
2020-10-20 16:04:29 +0000

    Deploying to gh-pages from  @ 67d06cfb4fe274d41cdc4e19fbf518f42f93bff4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.982854e0a726966be914aee23c4b3762.js"
+  "/glasswall-desktop/precache-manifest.92ae171e8a5b22f1d5ea1ec44edf4a35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glasswall-desktop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
