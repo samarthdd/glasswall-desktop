1ab6ffeb9018a7200b867a9686c011f9aa56daab
Mudit <gmuditdce@gmail.com>
2021-01-20 16:26:29 +0530

    added test to get report path

--- tests/index.test.js
+++ tests/index.test.js
@@ -22,32 +22,11 @@ describe('59934084', () => {
 
 });
 
+describe('to get report path', () => {
+  it('should render', () => {
+    let path = expect(Utils.getReportPath()).not.
+      toBe('null') ;
+  });
 
-// describe('59934084', () => {
-//   let wrapper;
-//   beforeEach(() => {
-//     wrapper = shallow(<SomeComponent></SomeComponent>);
-//   });
-//   it('should render', () => {
-//     expect(wrapper.exists).toBeTruthy();
-//   });
+});
 
-//   it('should handle submit, set status to true', () => {
-//     ipcRenderer.on.mockImplementationOnce((event, callback) => {
-//       callback(event, 'success');
-//     });
-//     wrapper.find('form').simulate('submit');
-//     expect(wrapper.state('status')).toBeTruthy();
-//     expect(ipcRenderer.on).toBeCalledWith('asynchronous-reply', expect.any(Function));
-//     expect(ipcRenderer.send).toBeCalledWith('update', 'value');
-//   });
-//   it('should handle submit without setting status to true', () => {
-//     ipcRenderer.on.mockImplementationOnce((event, callback) => {
-//       callback(event, 'failure');
-//     });
-//     wrapper.find('form').simulate('submit');
-//     expect(wrapper.state('status')).toBeFalsy();
-//     expect(ipcRenderer.on).toBeCalledWith('asynchronous-reply', expect.any(Function));
-//     expect(ipcRenderer.send).toBeCalledWith('update', 'value');
-//   });
-// });
