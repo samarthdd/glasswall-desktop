fbdbf4a5697952a0205362958d541b5c4fe8f080
Sanchit <rajmbcoderx@gmail.com>
2021-02-11 15:34:21 +0530

    Added more tests
    Added more tests

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,10 @@
 
     // Runs special logic, adding special
     // extended assertions to Jest
-    setupFilesAfterEnv: [
-      '@testing-library/jest-dom/extend-expect'
-    ],
-    // setupFiles: ["<rootDir>/jest.setup.js"],
+    // setupFilesAfterEnv: [
+    //   '@testing-library/jest-dom/extend-expect'
+    // ],
+    setupFiles: ["<rootDir>/src/setupTests.js"],
     // Test spec file resolution pattern
     // Matches parent folder `__tests__` and filename
     // should contain `test` or `spec`.
